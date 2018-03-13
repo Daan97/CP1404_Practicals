1b8064a0055a28f31a85956dccd3e164887990d8
Daan97 <daan.feltonbusch@my.jcu.edu.au>
2018-03-13 11:57:15 +1000

    Add ValueError and ZeroDivisionError

---
+++ prac_02/exceptions.py
@@ -0,0 +1,26 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+2. When will a ZeroDivisionError occur?
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+
+Answers:
+1. A ValueError will occur if one of the numbers is a float number
+2. A ZeroDivisionError occurs when the denominator is set to 0, as you can't divide by 0.
+3.
+"""
+
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+print("Finished.")
+
+
+
---
+++ prac_02/files.py
@@ -0,0 +1,32 @@
+# Do-from-scratch exercise
+
+# 1.
+name = str(input("What is your name: "))
+name_text_file = open('name.txt', 'w')
+print(name, file = name_text_file)
+name_text_file.close()
+
+#2.
+name_text_file = open('name.txt', 'r')
+print("Your name is", name)
+name_text_file.close()
+
+#3.
+numbers_text_file = open('numbers.txt', 'r')
+number1 = int(numbers_text_file.readline())
+number2 = int(numbers_text_file.readline())
+print("The result is", number1 + number2)
+numbers_text_file.close()
+
+
+#Original attempt
+# with open('numbers.txt', 'r') as numbers_text_file:
+#     total = 0
+#     for line in numbers_text_file:
+#         try:
+#             total += int(line)
+#         except ValueError:
+#             pass
+# print("The result is ",total)
+
+
