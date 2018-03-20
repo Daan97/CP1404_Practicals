a55cb095edff0c4c2d2acc880b3340ecc9fcd5e5
Daan97 <daan.feltonbusch@my.jcu.edu.au>
2018-03-20 12:25:10 +1000

    Check for username in list

---
+++ prac_04/security_checker.py
@@ -0,0 +1,10 @@
+# Program 2
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole',
+'InterpreterInterface', 'StartServer', 'bob']
+user_name = input("What is your username: ")
+if user_name in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
