57075e09cdfa2f7ec74a0dc40f02621a1341d6ec
Daan97 <daan.feltonbusch@my.jcu.edu.au>
2018-03-26 11:23:56 +1000

    Add quick_pick generator

---
+++ prac_04/quick_pick_generator.py
@@ -0,0 +1,35 @@
+import random
+
+numbers_per_line = 6
+minimum = 1
+maximum = 45
+
+
+def main():
+    number_of_quick_pick = int(input("How many quick picks? "))
+    while number_of_quick_pick < 0:
+        print("Invalid number")
+        number_of_quick_pick = int(input("How many quick picks? "))
+
+    for i in range(number_of_quick_pick):
+        quick_pick = []
+        for j in range(numbers_per_line):
+            number = random.randint(minimum, maximum)
+            while number in quick_pick:
+                number = random.randint(minimum, maximum)
+            quick_pick.append(number)
+        quick_pick.sort()
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
+
+main()
+
+
+# Original_attempt
+# quick_pick = int(input("How many quick picks? "))
+# for i in range(quick_pick):
+#     random_numbers = random.sample(range(1, 45), 6)
+#     print(" ".join(random_numbers[::])
+#
+# numbers = []
+# for i in range(1, 6):
